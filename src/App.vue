d57d409baf9caef509c20048f6886b9e1580ad2f
Renan Paiva Bonette <renan.bonette@gmail.com>
2019-02-02 20:50:51 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Countdown end="January 1, 2023"></Countdown>
+  </div>
+</template>
+
+<script>
+import Countdown from 'vuejs-countdown'
+
+export default {
+  components: { Countdown }
+}
+</script>
+
